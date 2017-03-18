283b9cd44ddcaa2d80442f4577ab31513bf39657
pychap <pychapster@gmail.com>
2017-03-18 07:21:13 -0700

    Final odd exercise

--- Easy_1/odd.rb
+++ Easy_1/odd.rb
@@ -1,10 +1,26 @@
+require 'pry'
+
+# def is_odd?(num)
+#   # binding.pry
+#   if num.to_i % 2 == 0
+#     puts "false"
+#   elsif num.to_i % 2 != 0    
+#     puts "true"
+#   end
+# end
+
 def is_odd?(num)
-  if num % 2 == 0
-    puts "false"
-  elsif num % 2 != 0    
-    puts "true"
+  convert = 0
+  if num < 0
+   convert = num * -1
+   convert.to_i.remainder(2) == 1
+   else
+   num.to_i.remainder(2) == 1
   end
+ 
 end
 
-puts is_odd?(4)
-puts is_odd?(7)
+puts is_odd?(2.2)  
+puts is_odd?(5.2)  
+puts is_odd?(-17)
+puts is_odd?(-8)
