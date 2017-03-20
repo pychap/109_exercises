5d34d9d1ebfd7a0edc2785ea2592788c39889ba0
pychap <pychapster@gmail.com>
2017-03-19 20:52:58 -0700

    Final solution for Reverse it II

--- Easy_1/reverse_it_2.rb
+++ Easy_1/reverse_it_2.rb
@@ -1,8 +1,21 @@
 require 'pry'
 
 def reverse_words(words)
+  word_array = words.split
+  reversed_arr = []
+  final_words = ''
 
-  
+  word_array.each do |word|
+    if word.size >= 5
+      reversed_arr << word.reverse
+    else
+      reversed_arr << word
+    end
+    final_words = reversed_arr.join(' ')
+  end
+  final_words
 end
 
+puts reverse_words('Professional')
 puts reverse_words('Walk around the block')
+puts reverse_words('Launch School')
