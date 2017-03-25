8e0a29e3c895a95e05d9f401899c4ea99015458d
pychap <pychapster@gmail.com>
2017-03-25 09:52:41 -0700

    Sum of Digits addition. /n Sum of digits exercise with further exploration.

---
+++ Easy_1/sum_of_digits.rb
@@ -0,0 +1,25 @@
+# method takes one argument
+# returns the sum of its digits
+# input: positive integer
+
+require 'pry'
+
+# def sum(nums)
+#   add_able = nums.digits
+#   total = 0
+
+#   add_able.each do |number|
+#    total += number
+#   end
+#   total
+# end
+
+# further exploration:
+def sum(num)
+  # num.to_s.chars.map! { |int| int.to_i }.reduce(:+)
+  num.to_s.chars.map! { |int| int.to_i }.sum
+end
+
+puts sum(23) == 5
+puts sum(496) == 19
+puts sum(123_456_789) == 45
